1e7abe3fabb6d132ce5ee027c74a123ffb35271e
witchwarlock666 <87949744+witchwarlock666@users.noreply.github.com>
2022-11-15 16:30:13 -0300

    Adocao

--- Include/scripts/script.js
+++ Include/scripts/script.js
@@ -19,4 +19,31 @@ const openCloseNav = () => {
     }
 }
 
+const changeImg = e => {
+    let arr = e.src.split('/')
+    console.log(arr)
+    let arr2 = arr[arr.length - 1].split('.')
+    console.log(arr2)
+    let val = arr2[0][arr2[0].length - 1]
+
+    if (val == 'a') {
+        val = 'b'
+    }
+    else {
+        val = 'a'
+    }
+
+    let arr3 = arr2[0].slice(0, -1)
+    arr3 += val
+    console.log(arr3)
+    arr2[0] = arr3
+    console.log(arr2)
+    arr2 = arr2.join('.')
+    arr[arr.length - 1] = arr2
+    arr = arr.join('/')
+    console.log(arr)
+
+    e.src = arr
+}
+
 const sleep = ms => new Promise(r => setTimeout(r, ms));
