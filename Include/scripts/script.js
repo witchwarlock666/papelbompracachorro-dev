d4aa9df87c9499bf7cbd36a25e2168a60df689d7
witchwarlock666 <87949744+witchwarlock666@users.noreply.github.com>
2022-11-15 23:55:44 -0300

    Remove debug code

--- Include/scripts/script.js
+++ Include/scripts/script.js
@@ -21,9 +21,7 @@ const openCloseNav = () => {
 
 const changeImg = e => {
     let arr = e.src.split('/')
-    console.log(arr)
     let arr2 = arr[arr.length - 1].split('.')
-    console.log(arr2)
     let val = arr2[0][arr2[0].length - 1]
 
     if (val == 'a') {
@@ -35,13 +33,10 @@ const changeImg = e => {
 
     let arr3 = arr2[0].slice(0, -1)
     arr3 += val
-    console.log(arr3)
     arr2[0] = arr3
-    console.log(arr2)
     arr2 = arr2.join('.')
     arr[arr.length - 1] = arr2
     arr = arr.join('/')
-    console.log(arr)
 
     e.src = arr
 }
